9d17cd37eeccd8467943bfbd7a516a340306110c
AndrewTtofi <andreas@ttofi.com>
2021-08-01 22:28:22 +0300

    Update VPC + Subnet

--- project1.tf
+++ project1.tf
@@ -1,6 +1,10 @@
 #for referrence https://www.youtube.com/watch?v=SLB_c_ayRMo&t=1226s&ab_channel=freeCodeCamp.org
 
 #Good to know note: whenever you change the region, the AMI ID changes as well
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
 resource "aws_instance" "first-virtual-machine" {
  ami           = "ami-0a8e758f5e873d1c1"
  instance_type = "t2.micro"
@@ -14,5 +18,24 @@ resource "aws_instance" "first-virtual-machine" {
     #In order to "stage" all the changes that are going to be made to your aws infrastructure you can type the command: terraform plan
     #In order to apply all the changes on AWS infrastructure you can type the command: terraform apply
     #In order to modify or change something on your aws infrastructure you just simply add or remove from your code and hit: terraform apply
-    #In order to delete the something you type in: terraform destroy
-    
+    #In order to delete the something you type in: terraform destroy (usually this is not run very often since its going to destroy everything)
+
+resource "aws_vpc" "first-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  tags = {
+    Name = "First-VPC"
+  }
+
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     =  aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "first-vpc-subnet-1"
+  }
+}
+
+#The order that you write your code does not really matter since it takes it as a whole
+  #e.g if we placed the subnet above the vpc it would still create the subnet under our vpc
