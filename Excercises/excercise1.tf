5995e9195d0508178cebb369b65c1bdaa1f783ab
AndrewTtofi <andreas.ttofi>
2021-08-17 14:51:03 +0300

    added security group rules and started ubuntu machine

--- Excercises/excercise1.tf
+++ Excercises/excercise1.tf
@@ -3,11 +3,44 @@ provider "AWS" {
   region = "${var.aws_region}"
 }
 
+# resource "aws_vpc" "WebServer" {
+#   cidr_block = "10.10.0.0/16"
+#   tags = {
+#     Name = "Excercise1WebServer"
+#   }
+# }
+
+
+
+#Create the ec2 instance
 resource "aws_instance" "ubuntu-server" {
    ami = "ami-0a8e758f5e873d1c1"
    instance_type = "t2.micro"
    availability_zone = "eu-west-1a"
    key_name = ""
-
    
+
+}
+
+
+#Create the security group
+resource "aws_security_group" "sg_ex1_server1" {
+    name = "${var.sgname}"
+}
+
+resource "aws_security_group_rule" "http_inbound" {
+    type = "ingress"
+    from_port = "${var.httpport}"
+    to_port = "${var.httpport}"
+    protocol = "tcp"
+    security_group_id = "${aws_security_group.sg_ex1_server1.id}"
+}
+
+resource "aws_security_group_rule" "https_inbound" {
+    type = "ingress"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_group_id = "${aws_security_group.sg_ex1_server1.id}"
+  
 }
